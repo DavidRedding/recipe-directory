006e1322ed7dbd192aff798c81a2d16d400fa1bb
David Redding <redding.david35@gmail.com>
2022-04-13 23:28:59 -0500

    routes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,32 @@
+import { BrowserRouter, Route, Switch, NavLink, Redirect } from 'react-router-dom';
+
+// page components
+import Home from './pages/home/Home';
+import Create from './pages/create/Create';
+import Recipe from './pages/recipe/Recipe';
+import Search from './pages/search/Search';
+
 const App = () => {
-  return <div className="min-h-screen">Hi</div>;
+  return (
+    <div className="min-h-screen">
+      <BrowserRouter>
+        <Switch>
+          <Route path="/" exact>
+            <Home />
+          </Route>
+          <Route path="/create">
+            <Create />
+          </Route>
+          <Route path="/search">
+            <Search />
+          </Route>
+          <Route path="/recipes/:id">
+            <Recipe />
+          </Route>
+        </Switch>
+      </BrowserRouter>
+    </div>
+  );
 };
 
 export default App;
