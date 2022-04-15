3598736250a4437a66229cf3d40c81b5f075f86c
David Redding <redding.david35@gmail.com>
2022-04-14 20:16:11 -0500

    Detailed view with (useParams). soft run.

--- src/components/RecipeList.js
+++ src/components/RecipeList.js
@@ -5,10 +5,10 @@ const RecipeList = ({ recipes }) => (
     {recipes.map((recipe) => (
       <div key={recipe.id} className=" p-4 bg-slate-100 rounded hover:rotate-6 transition-all duration-300">
         <h3 className="font-bold text-2xl text-gray-700">{recipe.title}</h3>
-        <p className="  text-gray-500 text-lg mt-1">{recipe.cookingTime} to make.</p>
-        <div className=" mt-2">{recipe.method.substring(0, 80)}...</div>
+        <p className="text-gray-500 text-lg mt-1">{recipe.cookingTime} to make.</p>
+        <div className=" mt-2">{recipe.method.substring(0, 95)}...</div>
         <Link
-          className="block text-center mt-4 py-1 bg-gray-300 w-5/12 mx-auto text-lg text-gray-700"
+          className="block text-center mt-4 bg-gray-300 mx-auto text-gray-700 w-28 p-2"
           to={`/recipes/${recipe.id}`}
         >
           Cook This
--- src/pages/recipe/Recipe.js
+++ src/pages/recipe/Recipe.js
@@ -1,5 +1,18 @@
+import { useParams } from 'react-router-dom';
+import useFetch from '../../hooks/useFetch';
+
 const Recipe = () => {
-  return <div>Recipe</div>;
+  const { id } = useParams(); // params.id
+  const url = `http://localhost:3000/recipes/${id}`;
+  const { data, error, isPending } = useFetch(url);
+
+  return (
+    <div>
+      {error && <div>{error}</div>}
+      {isPending && <div>Loading...</div>}
+      {data && <div>{data.title}</div>}
+    </div>
+  );
 };
 
 export default Recipe;
