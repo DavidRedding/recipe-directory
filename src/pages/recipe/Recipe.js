d5ff1353e73e692cb52a1057d95cf3e31d244d51
David Redding <redding.david35@gmail.com>
2022-05-04 19:43:27 -0500

    fetching a firestore document

--- src/pages/recipe/Recipe.js
+++ src/pages/recipe/Recipe.js
@@ -1,13 +1,34 @@
+import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
-import useFetch from '../../hooks/useFetch';
+import { projectFirestore } from '../../firebase/config';
 import { useTheme } from '../../hooks/useTheme';
 
 const Recipe = () => {
   const { id } = useParams(); // params.id
-  const url = `http://localhost:3000/recipes/${id}`;
-  const { data: recipe, error, isPending } = useFetch(url);
   const { mode } = useTheme();
   const dark = mode === 'dark';
+
+  const [recipe, setRecipe] = useState(null);
+  const [isPending, setIsPending] = useState(false);
+  const [error, setError] = useState(false);
+
+  useEffect(() => {
+    setIsPending(true);
+
+    projectFirestore
+      .collection('recipes')
+      .doc(id)
+      .get()
+      .then((doc) => {
+        if (doc.exists) {
+          setIsPending(false);
+          setRecipe(doc.data());
+        } else {
+          setIsPending(false);
+          setError('Could not find that recipe');
+        }
+      });
+  }, [id]);
 
   // prettier-ignore
   return (
